793a6b0e6573675ab4b2b5f53273ba21a62ba6cc
kevgbailey <kgbailey02@gmail.com>
2025-10-11 15:46:21 -0600

    restructure

---
+++ tandem-api/src/main/java/com/tandem/TandemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tandem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TandemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TandemBackendApplication.class, args);
+	}
+
+}
---
+++ tandem-api/src/test/java/com/tandem/TandemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tandem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TandemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
