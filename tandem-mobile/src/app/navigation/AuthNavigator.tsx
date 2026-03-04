2bc4fa7fa1052cdb963ec77ea4f041095f208737
savannah bjorkman <savbjork@byu.edu>
2026-03-04 22:30:01 +0100

    fix: register WelcomeScreen in AuthNavigator

--- tandem-mobile/src/app/navigation/AuthNavigator.tsx
+++ tandem-mobile/src/app/navigation/AuthNavigator.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { AuthStackParamList } from './types';
 
 // Placeholder screens - will be implemented later
+import { WelcomeScreen } from '@features/auth/screens/WelcomeScreen';
 import { SignInScreen } from '@features/auth/screens/SignInScreen';
 import { SignUpScreen } from '@features/auth/screens/SignUpScreen';
 
@@ -20,6 +21,7 @@ export const AuthNavigator: React.FC = () => {
         animation: 'slide_from_right',
       }}
     >
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="SignIn" component={SignInScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
     </Stack.Navigator>
